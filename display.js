8f3203325df620bd28b50f38492470d3c0ef192e
helenen <helene.niassy@gmail.com>
2019-10-03 16:23:13 +0200

    creata board

--- display.js
+++ display.js
@@ -2,9 +2,33 @@ import { createBoard } from "./index.js";
 
 function generateBoard(board) {
   board = createBoard();
+
+  // crée une div dans le DOM
+  const mainContainer = document.createElement("table");
+  const container = document.createElement("tbody");
+
+  // je donne une class à l'élément et je dis que container est son nom
+  mainContainer.setAttribute("class", "container");
+
+  // je dis au DOM que container est l'enfant de <body />
+  document.body.appendChild(mainContainer);
+  mainContainer.appendChild(container);
+
+  // fait
+  for (var i = 0; i < board.length; i++) {
+    const row = document.createElement("tr");
+    row.setAttribute("class", "row");
+    container.appendChild(row);
+
+    for (var j = 0; j < board[i].length; j++) {
+      const column = document.createElement("td");
+      column.setAttribute("class", "column");
+      row.appendChild(column);
+    }
+  }
 }
 
-generateBoard();
+generateBoard(createBoard());
 
 /* <table border="10" cellpadding="20" cellspacing="0">
       <tbody>
